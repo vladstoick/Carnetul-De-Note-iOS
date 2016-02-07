058db0dce9021ee16156ab48729434bd3e5f0730
Vlad Stoica <vlad96stoica@gmail.com>
2016-02-07 16:28:06 +0000

    Initial Commit

---
+++ Carnetul de Note/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Carnetul de Note
+//
+//  Created by Vlad Stoica on 07/02/16.
+//  Copyright © 2016 Vlad Stoica. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
